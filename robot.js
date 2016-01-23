b4b108890d56b82efbd4d2601f621c941a8f00a8
NatalyL <labunskaya.nataly@gmail.com>
2016-01-23 15:34:38 +0200

    level 4

--- robot.js
+++ robot.js
@@ -19,3 +19,14 @@ for (i = 0; i < 7; i++) {
     south();
     east();
 }
+
+//Level_4
+
+var i;
+for (i = 0; i < 13; i++) {
+    if (isFree('south')) {
+        south();
+    } else {
+        east();
+    }
+}
